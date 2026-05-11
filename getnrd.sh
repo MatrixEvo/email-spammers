d436cf3c93711d218a90170bfee1b34d705cd902
MatrixEvo <39986709+MatrixEvo@users.noreply.github.com>
2026-05-11 10:59:32 +0800

    Add SPF softfail strict blocklist download

--- getnrd.sh
+++ getnrd.sh
@@ -6,3 +6,4 @@ wget https://raw.githubusercontent.com/xRuffKez/NRD/refs/heads/main/lists/14-day
 wget https://raw.githubusercontent.com/MatrixEvo/email-spammers/refs/heads/main/gmail_spammers.txt -O "/ips1/blocklists/gmail_spammers.txt"
 wget https://raw.githubusercontent.com/MatrixEvo/email-spammers/refs/heads/main/spammers.txt -O "/ips1/blocklists/spammers.txt"
 wget https://raw.githubusercontent.com/MatrixEvo/email-spammers/refs/heads/main/ip_blocklist.txt -O "/ips1/blocklists/ip_blocklist.txt"
+wget https://raw.githubusercontent.com/MatrixEvo/email-spammers/refs/heads/main/spf_softfail_strict.txt -O "/ips1/blocklists/spf_softfail_strict.txt"
