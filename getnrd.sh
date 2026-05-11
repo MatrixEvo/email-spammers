13717cd8f04939453ff3c980cdd7be1f017c1ec9
MatrixEvo <39986709+MatrixEvo@users.noreply.github.com>
2026-05-11 12:34:04 +0800

    Replace phishing list URL with DGA list URL

--- getnrd.sh
+++ getnrd.sh
@@ -2,7 +2,7 @@
 
 # /ips1/blocklists/getnrd.sh
 
-wget https://raw.githubusercontent.com/xRuffKez/NRD/refs/heads/main/lists/14-day_phishing/domains-only/nrd-phishing-14day.txt -O "/ips1/blocklists/nrd-phishing-14day.txt"
+wget https://cdn.jsdelivr.net/gh/hagezi/dns-blocklists@latest/domains/dga30.txt -O "/ips1/blocklists/dga30.txt"
 wget https://raw.githubusercontent.com/MatrixEvo/email-spammers/refs/heads/main/gmail_spammers.txt -O "/ips1/blocklists/gmail_spammers.txt"
 wget https://raw.githubusercontent.com/MatrixEvo/email-spammers/refs/heads/main/spammers.txt -O "/ips1/blocklists/spammers.txt"
 wget https://raw.githubusercontent.com/MatrixEvo/email-spammers/refs/heads/main/ip_blocklist.txt -O "/ips1/blocklists/ip_blocklist.txt"
